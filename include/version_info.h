8302e1dfaa44326de0ac20c2fb629c09232dbd62
lashhev <lashhev-andrej@yandex.ru>
2020-08-02 22:18:59 +0300

    updated

--- include/version_info.h
+++ include/version_info.h
@@ -3,7 +3,7 @@
 
 #define LIB_NAME          "fun_learning"
 #define LIB_BUILD_TYPE    "DEVELOP"
-#define LIB_VERSION       "0.0.1"
-#define LIB_DATE          "29-07-2020"
+#define LIB_VERSION       "0.0.8"
+#define LIB_DATE          "02-08-2020"
 
 #endif // __VERSION_INFO_H
