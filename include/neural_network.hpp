f6de24f7518af4255c9f11a0c1a3dfcd46014050
lashhev <lashhev-andrej@yandex.ru>
2020-08-02 20:58:09 +0300

    1. NeuralNetwork class was implemented 2. Backpropagation algorithm was implemented

--- include/neural_network.hpp
+++ include/neural_network.hpp
@@ -2,25 +2,41 @@
 #define __NEURAL_NETWORK_HPP__
 
 #include "neural_layer.hpp"
+#include <vector>
 namespace fun_learning
 {
 class NeuralNetwork
 {
 public:
-    struct Parameters
-    {
-        uint16_t inputs_number;
-        std::vector<uint16_t> nodes_in_layer;
-        uint16_t layers_number;
-        uint16_t outputs_number;
-        std::string activation_func_type;
-    };
+    // struct Parameters
+    // {
+    //     uint16_t inputs_number;
+    //     std::vector<uint16_t> nodes_in_layer;
+    //     uint16_t layers_number;
+    //     uint16_t outputs_number;
+    //     std::string activation_func_type;
+    // };
 
     NeuralNetwork();
-    NeuralNetwork(const Parameters &params);
+    // NeuralNetwork(const Parameters &params);
 
-    void add(const Layer &layer);
-    void insert(uint16_t i, const Layer &layer);
+    void feed_forward(const Eigen::MatrixXd & input_values, Eigen::MatrixXd & results);
+    void feed_forward_and_remember(const Eigen::MatrixXd & input_values, std::vector<Eigen::MatrixXd>& each_layers_outputs_);
+
+    void add(const NeuralLayer &layer);
+    void insert(uint16_t i, const NeuralLayer &layer);
+    void remove(uint16_t i);
+    void pop_back();
+    void train(const Eigen::MatrixXd & input_values, 
+                                        const Eigen::MatrixXd & target_result, 
+                                        double learning_scale, double fval);
+private:
+    void back_propogation__(const Eigen::RowVectorXd & input_values, 
+                                        const Eigen::RowVectorXd & target_result, 
+                                        double learning_scale, double fval);                                   
+private:
+    std::vector<NeuralLayer> __layers;
+    // Parameters __params;
 };
 } // fun_learning
 #endif //__NEURAL_NETWORK_HPP__
---
+++ src/neural_network.cpp
@@ -0,0 +1,153 @@
+#include "neural_network.hpp"
+#include "activation_fuctions.hpp"
+
+using namespace Eigen;
+using namespace std;
+using namespace fun_learning;
+
+
+NeuralNetwork::NeuralNetwork()
+{
+
+}
+
+void NeuralNetwork::add(const NeuralLayer &layer)
+{
+    __layers.push_back(layer);
+}
+
+void NeuralNetwork::feed_forward(const MatrixXd & input_values, MatrixXd & results)
+{
+    results = input_values;
+    for(auto layer:__layers)
+        results = results*layer;
+}
+
+void NeuralNetwork::feed_forward_and_remember(const MatrixXd & input_values, std::vector<Eigen::MatrixXd>& each_layers_outputs_)
+{
+    MatrixXd results = MatrixXd(input_values);
+    each_layers_outputs_.push_back(results);
+    for(auto layer:__layers)
+    {
+        results = results*layer;
+        each_layers_outputs_.push_back(results);
+    }
+}
+
+void NeuralNetwork::insert(uint16_t i, const NeuralLayer &layer)
+{
+    __layers.insert(__layers.begin()+i, layer);
+}
+
+void NeuralNetwork::remove(uint16_t i)
+{
+    __layers.erase(__layers.begin()+i);
+}
+
+void NeuralNetwork::pop_back()
+{
+    __layers.pop_back();
+}
+
+void NeuralNetwork::train(const MatrixXd & input_values, 
+                                        const MatrixXd & target_result, 
+                                        double learning_scale, double fval)
+{
+    for(uint16_t i=0; i < input_values.rows();i++)
+    {
+        back_propogation__(input_values.row(i), target_result.row(i), learning_scale, fval);
+    }
+    std::cout << "Training is finished! " << std::endl;
+}
+
+
+void NeuralNetwork::back_propogation__(const RowVectorXd & input_values, 
+                                        const RowVectorXd & target_result, 
+                                        double learning_scale, double fval)
+{
+    double e_t = fval+50;
+    uint32_t cycle_num = 0;
+    vector<MatrixXd> each_layers_out;
+    MatrixXd results;
+    while (e_t > fval)
+    {
+        cycle_num++;
+        feed_forward_and_remember(input_values, each_layers_out);
+        results = each_layers_out.back();
+        e_t = (0.5*(target_result - results).array().square()).sum();
+        std::cout << "cycle_num: " << cycle_num << std::endl << "Neuron precision: " << e_t << std::endl;
+        
+        ArrayXXd E, dR, inputs, delta;
+        Eigen::MatrixXd weights, new_weights;
+        uint16_t layers_num = __layers.size() - 1;
+        for(uint16_t i=layers_num; i > 0;i--)
+        {
+            /// Process output layer
+            results = each_layers_out[i+1];
+            __layers[i].get_weights(weights);
+            if (i == layers_num)
+                E = -(target_result-results).array();
+            else
+                E = delta.matrix()*weights;
+        
+            dR = sigmoid_derivative(results).array();
+       
+            inputs = each_layers_out[i].array();
+            
+            delta = ArrayXXd(E*dR);
+        
+            new_weights = weights.array() - learning_scale*delta.replicate(weights.rows(), 1)*inputs.array().transpose().replicate(1, weights.cols());
+            __layers.back().set_weights(new_weights);
+        }
+    } 
+}
+
+
+
+// void NeuralNetwork::back_propogation(const Eigen::RowVectorXd & input_values, 
+//                                         Eigen::RowVectorXd & target_result, 
+//                                         double learning_scale, double fval)
+// {
+//     double e_t = fval+50;
+//     uint32_t cycle_num = 0;
+//     vector<MatrixXd> each_layers_out;
+//     MatrixXd results;
+//     while (e_t > fval)
+//     {
+//         cycle_num++;
+//         feed_forward_and_remember(input_values, each_layers_out);
+//         results = each_layers_out.back();
+//         e_t = (0.5*(target_result - results).array().square()).sum();
+//         std::cout << "cycle_num: " << cycle_num << std::endl << "Neuron precision: " << e_t << std::endl;
+//         // std::cout << "cicle_num: " << cycle_num << std::endl << "Neuron precision: " << e_t << std::endl;
+
+//         /// Process output layer
+//         ArrayXXd E = -(target_result-results).array();
+//         // printM(E, "E");
+//         ArrayXXd dR = sigmoid_derivative(results).array();
+//         // printM(dR, "dR");
+//         ArrayXXd inputs = (*(each_layers_out.end()-2)).array();
+//         // printM(inputs, "inputs");
+//         ArrayXXd delta = ArrayXXd(E*dR);
+//         // printM(delta, "delta");
+//         MatrixXd weights;
+//         __layers.back().get_weights(weights);
+//         Eigen::MatrixXd new_weights = weights.array() - learning_scale*delta.replicate(weights.rows(), 1)*inputs.array().transpose().replicate(1, weights.cols());
+//         __layers.back().set_weights(new_weights);
+//         // printM(new_weights, "new_weights");
+
+//         /// Process hidden layer
+//         results = *(each_layers_out.end()-2);
+//         (*(__layers.end()-2)).get_weights(weights);
+//         E = delta.matrix()*weights;
+//         dR = sigmoid_derivative(results).array();
+//         inputs = (*(each_layers_out.end()-3)).array();
+//         delta = ArrayXXd(E*dR);
+//         new_weights = weights.array() - learning_scale*delta.replicate(weights.rows(), 1)*inputs.array().transpose().replicate(1, weights.cols());
+//         (*(__layers.end()-2)).set_weights(new_weights);
+
+//     } 
+//     std::cout << "Training is finished! " << std::endl;
+//     feed_forward(input_values, results);
+//     printM(results, "results");
+// }
